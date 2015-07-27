e0368766486bb8cf7c694e57a0183dddb4a26f4e
Stewart McCoy <mccoy.stewart@gmail.com>
2015-07-27 10:24:05 -0700

    refactor animatePoints function to call revealPoints as an outside function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,20 @@
 var points = document.getElementsByClassName('point');
 
+function revealPoint(i) {
+    points[i].style.opacity = 1;
+    points[i].style.transform = "scaleX(1) translateY(0)";
+    points[i].style.msTransform = "scaleX(1) translateY(0)";
+    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+function reavealPoints() {
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
+};
+
 var animatePoints = function(i) {
-    (function revealPoint() {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }());
+    reavealPoints();
 };
 
 window.onload = function() {
@@ -22,3 +30,8 @@ window.onload = function() {
     });
 
 };
+
+
+
+
+
